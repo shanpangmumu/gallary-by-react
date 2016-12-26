6ed4919c68f2cea914624c375e3abce4f7f0bc53
zhangqi <296493868@qq.com>
2016-12-26 14:35:12 +0800

    lalala

---
+++ src/components/Main.js
@@ -0,0 +1,26 @@
+require('normalize.css/normalize.css');
+require('styles/App.css');
+
+import React from 'react';
+
+let yeomanImage = require('../images/yeoman.png');
+
+let data=require('./data/jsondata.json');
+console.info(data);
+
+class AppComponent extends React.Component {
+  render() {
+    return (
+      <div className="index">
+        <img src={yeomanImage} alt="Yeoman Generator" />
+        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
+      <span> "let us go"</span>
+      </div>
+    );
+  }
+}
+
+AppComponent.defaultProps = {
+};
+
+export default AppComponent;
